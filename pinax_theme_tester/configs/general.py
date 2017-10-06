f24fb5e9d0f2652ea3dec6d600067339e793fc24
Patrick Altman <paltman@gmail.com>
2017-10-05 21:58:05 -0500

    Add pagination

--- pinax_theme_tester/configs/general.py
+++ pinax_theme_tester/configs/general.py
@@ -2,13 +2,23 @@ from django.conf.urls import url, include
 
 from .base import ViewConfig
 
+page_obj = {
+    "has_previous": True,
+    "has_next": False,
+    "previous_page_number": 1,
+    "next_page_number": 10,
+    "number": 5
+}
+paginator = {
+    "page_range": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+}
 
 label = "general"
 title = "General"
 views = [
     ViewConfig(pattern=r"^general/404/$", template="404.html", name="general_400", pattern_kwargs={}),
     ViewConfig(pattern=r"^general/500/$", template="500.html", name="general_500", pattern_kwargs={}),
-    ViewConfig(pattern=r"^general/fragments/$", template="fragments.html", name="general_fragments", pattern_kwargs={}),
+    ViewConfig(pattern=r"^general/fragments/$", template="fragments.html", name="general_fragments", pattern_kwargs={}, is_paginated=True, page_obj=page_obj, paginator=paginator),
 ]
 urlpatterns = [
     view.url()
