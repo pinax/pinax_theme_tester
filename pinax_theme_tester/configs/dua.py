b100dfc553cd3e6fcaa527782d8fb9aa13e6aff5
Patrick Altman <paltman@gmail.com>
2017-10-14 17:00:29 -0500

    Tidy up DUA styles

--- pinax_theme_tester/configs/dua.py
+++ pinax_theme_tester/configs/dua.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
 
-from account.forms import LoginUsernameForm, SignupForm, ChangePasswordForm, SettingsForm, PasswordResetForm
+from account.forms import LoginUsernameForm, SignupForm, ChangePasswordForm, SettingsForm, PasswordResetForm, PasswordResetTokenForm
 
 from .base import ViewConfig
 
@@ -14,7 +14,7 @@ views = [
     ViewConfig(pattern=r"^account/confirm_email/(?P<key>\w+)/$", template="account/email_confirm.html", name="account_confirm_email", pattern_kwargs={"key": "abc"}, confirmation={"key": "foo", "email_address": {"email": "example@sample.com"}}),
     ViewConfig(pattern=r"^account/password/$", template="account/password_change.html", name="account_password", pattern_kwargs={}, form=ChangePasswordForm(user=None)),
     ViewConfig(pattern=r"^account/password/reset/$", template="account/password_reset.html", name="account_password_reset", pattern_kwargs={}, form=PasswordResetForm()),
-    ViewConfig(pattern=r"^account/password/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$", template="account/password_reset_token.html", name="account_password_reset_token", pattern_kwargs={"uidb36": "aaa", "token": "123"}, form=PasswordResetForm()),
+    ViewConfig(pattern=r"^account/password/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$", template="account/password_reset_token.html", name="account_password_reset_token", pattern_kwargs={"uidb36": "aaa", "token": "123"}, form=PasswordResetTokenForm()),
     ViewConfig(pattern=r"^account/settings/$", template="account/settings.html", name="account_settings", pattern_kwargs={}, form=SettingsForm()),
     ViewConfig(pattern=r"^account/delete/$", template="account/delete.html", name="account_delete", pattern_kwargs={}),
 ]
