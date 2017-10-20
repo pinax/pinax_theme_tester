8771703e496cbb0aca30426fb4582bc10de85b19
Patrick Altman <paltman@gmail.com>
2017-10-20 01:33:55 -0500

    More work on pinax-cohorts styling

--- pinax_theme_tester/configs/cohorts.py
+++ pinax_theme_tester/configs/cohorts.py
@@ -3,9 +3,9 @@ from django.urls import reverse
 from django.utils import timezone
 
 from pinax.cohorts.forms import CohortCreateForm
+from account.hooks import hookset
 
 from .base import ViewConfig as BaseViewConfig
-
 
 cohort = {
     "pk": 1,
@@ -15,10 +15,10 @@ cohort = {
         dict(email="foo@bar.com"),
         dict(email="foo@bar.com", user="username"),
         dict(email="foo@bar.com", user="username"),
-        dict(email="foo@bar.com", invited=True),
+        dict(email="foo@bar.com", invited=True, signup_code=dict(code=hookset.generate_signup_code_token(email="foo@bar.com"), expiry=timezone.now(), sent=timezone.now(), created=timezone.now())),
         dict(email="foo@bar.com"),
         dict(email="foo@bar.com"),
-        dict(email="foo@bar.com", invited=True),
+        dict(email="foo@bar.com", invited=True, signup_code=dict(code=hookset.generate_signup_code_token(email="foo@bar.com"), expiry=timezone.now(), sent=timezone.now(), created=timezone.now())),
         dict(email="foo@bar.com", user="username"),
         dict(email="foo@bar.com")
     ],
@@ -30,6 +30,17 @@ cohort_list = [
     cohort,
     cohort
 ]
+waiting_list = [
+    dict(email="foo@bar.com", created=timezone.now(), pk=1),
+    dict(email="foo@bar.com", created=timezone.now(), pk=1),
+    dict(email="foo@bar.com", created=timezone.now(), pk=1),
+    dict(email="foo@bar.com", created=timezone.now(), pk=1),
+    dict(email="foo@bar.com", created=timezone.now(), pk=1),
+    dict(email="foo@bar.com", created=timezone.now(), pk=1),
+    dict(email="foo@bar.com", created=timezone.now(), pk=1),
+    dict(email="foo@bar.com", created=timezone.now(), pk=1)
+]
+
 
 patch = "http://pinaxproject.com/pinax-design/patches/blank.svg"
 label = "cohort"
@@ -46,7 +57,7 @@ class ViewConfig(BaseViewConfig):
 views = [
     ViewConfig(pattern=r"^pinax-cohorts/$", template="pinax/cohorts/cohort_list.html", name="list", pattern_kwargs={}, cohorts=cohort_list),
     ViewConfig(pattern=r"^pinax-cohorts/create/$", template="pinax/cohorts/cohort_create.html", name="create", pattern_kwargs={}, form=CohortCreateForm()),
-    ViewConfig(pattern=r"^pinax-cohorts/(?P<pk>\d+)/$", template="pinax/cohorts/cohort_detail.html", name="detail", pattern_kwargs={"pk": 1}, cohort=cohort),
+    ViewConfig(pattern=r"^pinax-cohorts/(?P<pk>\d+)/$", template="pinax/cohorts/cohort_detail.html", name="detail", pattern_kwargs={"pk": 1}, cohort=cohort, waiting_list=waiting_list),
     ViewConfig(pattern=r"^pinax-cohorts/(?P<pk>\d+)/add_member/$", template=None, name="member_add", pattern_kwargs={"pk": 1}, menu=False),
     ViewConfig(pattern=r"^pinax-cohorts/(?P<pk>\d+)/send_invitiations/$", template=None, name="send_invitations", pattern_kwargs={"pk": 1}, menu=False)
 ]
