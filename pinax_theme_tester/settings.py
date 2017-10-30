d7f4acccf6c76acc3973f6ae7d0a7be0c788d66b
Patrick Altman <paltman@gmail.com>
2017-10-30 10:57:47 -0500

    Turn debug off in production

--- pinax_theme_tester/settings.py
+++ pinax_theme_tester/settings.py
@@ -5,7 +5,7 @@ PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir
 PACKAGE_ROOT = os.path.abspath(os.path.dirname(__file__))
 BASE_DIR = PACKAGE_ROOT
 
-DEBUG = True
+DEBUG = bool(int(os.environ.get("DEBUG", 1)))
 
 DATABASES = {
     "default": {
